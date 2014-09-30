2771241971c3f8a66631b60caa19431c6cab5df1
Satoshi Nakamoto <satoshin@gmx.com>
2014-10-01 00:14:41 +0200

    first commit

---
+++ workspace/Dummy-build/src/CMakeFiles/gold_app_dummy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gold_app_dummy.dir/dummy.cpp.o"
+  "/home/alox/.config/gold-4.6.0/plugins/libgold_app_dummy.pdb"
+  "/home/alox/.config/gold-4.6.0/plugins/libgold_app_dummy.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gold_app_dummy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ workspace_backup/Dummy-build/src/CMakeFiles/gold_app_dummy.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alox/Tesi/workspace/Dummy/src/dummy.cpp" "/home/alox/Tesi/workspace/Dummy-build/src/CMakeFiles/gold_app_dummy.dir/dummy.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "HAVE_BOOST_ZLIB"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/alox/Tesi/workspace/Dummy/src"
+  "/opt/gold-4.6/include/gold-4.6.0/Config"
+  "/opt/gold-4.6/include/gold-4.6.0"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
